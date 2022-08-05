c7c58a0283352e31137740f8f010ce80da5eb4b2
Nick Galante <nicholasgalante1997@gmail.com>
2022-08-04 23:25:12 -0600

    rename nodemailer example

--- packages/react-email-server/src/index.ts
+++ packages/react-email-server/src/index.ts
@@ -2,29 +2,9 @@ import { createServer, IncomingMessage, ServerResponse } from 'http';
 import { createTransport } from 'nodemailer';
 import Mail from 'nodemailer/lib/mailer';
 
-type RustMockExceptionableFunctionResolution = { statusCode: string; internalKey: string; }
-
-type EmailSendResponse = {
-    ok?: () => RustMockExceptionableFunctionResolution;
-    err?: (err: Error) => RustMockExceptionableFunctionResolution;
-};
-
-type EmailConstruct = {
-    subject: string;
-    sender: string;
-    recipient: string;
-    send: (mailOptions: Mail.Options) => EmailSendResponse;
-};
-
-class EmailEntity {
-    constructor() {}
-}
-
 const PORT = process.env.PORT || 4501 as const;
 
 const style = `color: cyan; margin: 4rem; font-family: serif; font-style: italic;`
-
-const handleSendEmail = (email: EmailConstruct) => {};
 
 const server = createServer((req: IncomingMessage, res: ServerResponse) => {
     res.statusCode = 200;
